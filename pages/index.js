24dbbb2830e2f68d1c122ded2d9a4a219c2113a0
Brian3647 <victoris3647@gmail.com>
2021-04-21 20:09:32 +0200

    Test

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to TakoYaki Radio!</h1>
-
         <p className={styles.description}>Open source music radio player</p>
-
+        Pull request test!
         {/*<div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
