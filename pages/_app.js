71b4f433a3071240d93a89177074cbd73414b5fa
Brian3647 <victoris3647@gmail.com>
2021-04-23 19:42:23 +0200

    Add vercel.json and FMT

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,11 @@ import styles from "../styles/globals.css";
 import { Provider } from "next-auth/client";
 
 function App({ Component, pageProps }) {
-  return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+	return (
+		<Provider session={pageProps.session}>
+			<Component {...pageProps} />
+		</Provider>
+	);
 }
 
 export default App;
