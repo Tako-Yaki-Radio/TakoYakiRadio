da5c856ea3d7c8b9e86f16fe9971a9c9096f40e8
Brian3647 <73384214+Brian3647@users.noreply.github.com>
2021-04-24 11:54:42 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,6 @@ import { useSession } from "next-auth/client";
 import Link from "next/link";
 
 export default function Home() {
-  console.log("Preview mode: ", process.env.IS_PREVIEW_SETUP || "No");
-
-  if (process.env.IS_PREVIEW_SETUP === "yes")
-    <meta http-equiv="refresh" content="2;url=/preview-github" />;
-
   const [session] = useSession();
 
   if (!process.env.DATABASE_URL || !process.env.NEXTAUTH_URL) {
